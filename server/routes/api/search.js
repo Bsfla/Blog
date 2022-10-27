88b55e2f5dfff1501ee5c64f447efb5cd3c0b364
baebumsoo <baebeomsu77@gmail.com>
2022-10-28 02:03:39 +0900

    Feat(bumsoo/server): 검색 api구현

---
+++ server/routes/api/search.js
@@ -0,0 +1,20 @@
+import express from "express";
+import Post from "../../models/post";
+
+const router = express.Router();
+
+router.get("/:searchTerm", async (req, res, next) => {
+  try {
+    const result = await Post.find({
+      title: {
+        $regex: req.params.searchTerm,
+        $options: "i",
+      },
+    });
+    console.log(result, "Search result");
+    res.send(result);
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
