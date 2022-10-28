4e39abbe70a692ee2183c8dd659e023007ca05ec
baebumsoo <baebeomsu77@gmail.com>
2022-10-28 19:34:04 +0900

    Feat(bumsoo/server): 패스워드 변경 api 추가

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -18,7 +18,7 @@ router.get("/", async (req, res) => {
   try {
     const users = await User.find();
     if (!users) throw Error("No users");
-    res.status(200).json(users, {success: "회원가입에 성공하셨습니다"});
+    res.status(200).json(users, { success: "회원가입에 성공하셨습니다" });
   } catch (e) {
     console.log(e);
     res.status(400).json({ msg: e.message });
@@ -72,6 +72,41 @@ router.post("/", (req, res) => {
       });
     });
   });
+});
+
+router.post("/:userName/profile", async (req, res) => {
+  try {
+    const { previousPassword, password, rePassword, userId } = req.body;
+    console.log(req.body, "userName Profile");
+    const result = await User.findById(userId, "password");
+
+    bcrypt.compare(previousPassword, result.password).then((isMatch) => {
+      if (!isMatch) {
+        return res.status(400).json({
+          match_msg: "기존 비밀번호와 일치하지 않습니다.",
+        });
+      } else {
+        if (password === rePassword) {
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+              if (err) throw err;
+              result.password = hash;
+              result.save();
+            });
+          });
+          res
+            .status(200)
+            .json({ success_msg: "비밀번호 업데이트에 성공했습니다." });
+        } else {
+          res
+            .status(400)
+            .json({ fail_msg: "새로운 비밀번호가 일치하지 않습니다." });
+        }
+      }
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 export default router;
