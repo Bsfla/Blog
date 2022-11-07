b1955b2bb145a6f7053b8232eddec3bb919fa756
baebumsoo <baebeomsu77@gmail.com>
2022-11-07 19:19:35 +0900

    Modify(bumsoo/client): 다이나믹 라우팅으로 페이지 라우팅 방식 변경

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,9 +1,67 @@
 import React from "react";
 import styled from "styled-components";
-import { MdClose } from 'react-icons/md';
+import { MdClose } from "react-icons/md";
+
+const Modal = ({
+  onChange,
+  onSubmit,
+  form,
+  localMessage,
+  open,
+  setOpen,
+  isAuth,
+}) => {
+  return (
+    <ModalStyle open={open}>
+      <ModalWrapper>
+        <ModalHeader>
+          <h3>{isAuth.login ? "Login" : "Register"}</h3>
+          <MdClose size={20} onClick={() => setOpen(false)} />
+        </ModalHeader>
+        <ModalForm onSubmit={onSubmit}>
+          {localMessage.length ? (
+            <AlertErrorMsg>
+              <span>{localMessage}</span>
+            </AlertErrorMsg>
+          ) : null}
+          {isAuth.register ? (
+            <>
+              {" "}
+              <label>Name</label>
+              <input
+                placeholder="Name"
+                name="name"
+                value={form.name}
+                onChange={onChange}
+              />{" "}
+            </>
+          ) : null}
+          <label>Email</label>
+          <input
+            placeholder="Email"
+            name="email"
+            value={form.email}
+            onChange={onChange}
+          />
+          <label>Password</label>
+          <input
+            placeholder="Password"
+            name="password"
+            type="password"
+            value={form.password}
+            onChange={onChange}
+          />
+          <SubmitButton>{isAuth.login ? "Login" : "Register"}</SubmitButton>
+        </ModalForm>
+      </ModalWrapper>
+    </ModalStyle>
+  );
+};
+
+export default Modal;
 
 const ModalStyle = styled.div`
-  display: ${(props) => props.open ? 'flex' : 'none'};
+  display: ${(props) => (props.open ? "flex" : "none")};
   justify-content: center;
   width: 100vw;
   height: 100vh;
@@ -14,15 +72,14 @@ const ModalStyle = styled.div`
   position: fixed;
   z-index: 999;
   background-color: rgba(0, 0, 0, 0.6);
-`
+`;
 const ModalWrapper = styled.div`
   width: 500px;
   height: auto;
   background-color: white;
   position: fixed;
   top: 30px;
-
-`
+`;
 const ModalHeader = styled.div`
   width: 100%;
   display: flex;
@@ -31,40 +88,37 @@ const ModalHeader = styled.div`
   border-bottom: 1px solid;
 
   h3 {
-      flex: 1;
+    flex: 1;
   }
   svg {
-      cursor: pointer;
+    cursor: pointer;
   }
-
-`
-const ModalForm = styled.form `
+`;
+const ModalForm = styled.form`
   width: 100%;
   height: 100%;
   padding: 20px 20px 0px 20px;
 
   label {
-      display: block;
+    display: block;
   }
 
   input {
-      width: 35em;
-      padding: 10px;
-      margin-top: 10px;
+    width: 35em;
+    padding: 10px;
+    margin-top: 10px;
   }
-  
-`
+`;
 const SubmitButton = styled.button`
-   background-color:#212529;
-   color: white;
-   width: 35em;
-   padding: 15px;
-   margin: 30px 0px;
-   border: none;
-   outline: none;
-   cursor: pointer;
-   
-`
+  background-color: #212529;
+  color: white;
+  width: 35em;
+  padding: 15px;
+  margin: 30px 0px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`;
 const AlertErrorMsg = styled.div`
   background-color: #f8bbd0;
   width: 45em;
@@ -73,30 +127,4 @@ const AlertErrorMsg = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 20px;
-`
-
-
-const Modal = ({ onChange, onSubmit, form, localMessage, open, setOpen, isAuth }) => {
-    return (
-        <ModalStyle open={open} >
-            <ModalWrapper>
-               <ModalHeader>
-                  <h3>{isAuth.login ? 'Login' : 'Register'}</h3>
-                  <MdClose size={20} onClick={() => setOpen(false)}/>
-               </ModalHeader>
-               <ModalForm onSubmit={onSubmit}>
-               {localMessage.length ? <AlertErrorMsg><span>{localMessage}</span></AlertErrorMsg>  : null}
-               {isAuth.register ? <> <label>Name</label>
-                  <input placeholder="Name" name='name' value={form.name} onChange={onChange} /> </> : null} 
-                  <label>Email</label>
-                  <input placeholder="Email" name='email' value={form.email} onChange={onChange} />
-                  <label>Password</label>
-                  <input placeholder="Password" name='password' type='password' value={form.password} onChange={onChange} />
-                  <SubmitButton>{isAuth.login ? 'Login' : 'Register'}</SubmitButton>
-               </ModalForm>
-            </ModalWrapper>
-        </ModalStyle>
-    )
-}
-
-export default Modal;
+`;
--- client/src/routes/Routing.js
+++ client/src/routes/Routing.js
@@ -1,23 +1,27 @@
+import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import PostWritePage from "../pages/Post/PostWritePage";
-import PostDetailPage from "../pages/Post/PostDetailPage";
-import PostEditPage from "../pages/Post/PostEditPage";
-import CategoryPage from "../pages/CategoryPage";
-import SearchPage from "../pages/SearchPage";
-import ProfilePage from "../pages/ProfilePage";
+
+const HomePage = lazy(() => import("../pages/HomePage"));
+const PostWritePage = lazy(() => import("../pages/Post/PostWritePage"));
+const PostDetailPage = lazy(() => import("../pages/Post/PostDetailPage"));
+const PostEditPage = lazy(() => import("../pages/Post/PostEditPage"));
+const CategoryPage = lazy(() => import("../pages/CategoryPage"));
+const SearchPage = lazy(() => import("../pages/SearchPage"));
+const ProfilePage = lazy(() => import("../pages/ProfilePage"));
 
 const Routing = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/postwrite" element={<PostWritePage />} />
-      <Route path="/post/:id" element={<PostDetailPage />} />
-      <Route path="/post/:id/edit" element={<PostEditPage />} />
-      <Route path="/category/:categoryResult" element={<CategoryPage />} />
-      <Route path="/search/:searchResult" element={<SearchPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
-    </Routes>
+    <Suspense fallback={<div>Loading</div>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/postwrite" element={<PostWritePage />} />
+        <Route path="/post/:id" element={<PostDetailPage />} />
+        <Route path="/post/:id/edit" element={<PostEditPage />} />
+        <Route path="/category/:categoryResult" element={<CategoryPage />} />
+        <Route path="/search/:searchResult" element={<SearchPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </Suspense>
   );
 };
 
